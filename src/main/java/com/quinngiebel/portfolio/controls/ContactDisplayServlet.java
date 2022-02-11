52add301819b6a422db201aa7e85d669727f3de0
qtgiebel <qtgiebel@gmail.com>
2022-02-10 19:23:03 -0600

    add class comment

--- src/main/java/com/quinngiebel/portfolio/controls/ContactDisplayServlet.java
+++ src/main/java/com/quinngiebel/portfolio/controls/ContactDisplayServlet.java
@@ -5,6 +5,11 @@ import javax.servlet.http.*;
 import javax.servlet.annotation.*;
 import java.io.IOException;
 
+/**
+ * This class forwards to a jsp page.
+ *
+ * @author Quinn Giebel
+ */
 @WebServlet(name = "ContactDisplayServlet", value = "/contact")
 public class ContactDisplayServlet extends HttpServlet {
     @Override
