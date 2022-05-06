4fcee59603a8aec23ad11a917e73d3413af21649
qtgiebel <qtgiebel@gmail.com>
2022-05-06 12:22:37 -0500

    created url level login verification

---
+++ src/main/java/com/quinngiebel/admin/controls/LoginFilter.java
@@ -0,0 +1,41 @@
+package com.quinngiebel.admin.controls;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import java.io.IOException;
+
+import javax.servlet.*;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+@WebFilter("/admin/*")
+public class LoginFilter implements Filter {
+    private final Logger logger = LogManager.getLogger(this.getClass());
+
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+//        Filter.super.init(filterConfig);
+    }
+
+    @Override
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+        HttpServletRequest request = (HttpServletRequest) servletRequest;
+        HttpServletResponse response = (HttpServletResponse) servletResponse;
+        HttpSession session = request.getSession(false);
+
+        if (session == null || session.getAttribute("verifiedUser") == null) {
+            response.sendRedirect(request.getContextPath() + "/logIn");
+            return;
+        }
+
+        filterChain.doFilter(servletRequest, servletResponse);
+    }
+
+    @Override
+    public void destroy() {
+        Filter.super.destroy();
+    }
+}
