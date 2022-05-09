e22e295f088e48ce65ca2be3260fccc6437201e4
qtgiebel <qtgiebel@gmail.com>
2022-05-09 01:32:38 -0500

    add tojson fuction

--- src/main/java/com/quinngiebel/admin/persistence/CategoryDao.java
+++ src/main/java/com/quinngiebel/admin/persistence/CategoryDao.java
@@ -1,7 +1,9 @@
 package com.quinngiebel.admin.persistence;
 
 import com.quinngiebel.admin.entities.Category;
-import org.hibernate.mapping.Set;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 public class CategoryDao extends GenericDao<Category> {
 
@@ -9,4 +11,13 @@ public class CategoryDao extends GenericDao<Category> {
         this.setType(Category.class);
     }
 
+    /**
+     * Returns a JSON representation of the list of pieces.
+     * @param resultList The list of pieces.
+     * @return A JSON representation of the list
+     */
+    public String toJSON(List<Category> resultList) {
+        return String.format("{\"categories\":[%s]}",
+                resultList.stream().map(Category::toJSON).collect(Collectors.joining(",")));
+    }
 }
