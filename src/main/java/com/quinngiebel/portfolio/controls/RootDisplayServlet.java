8f9f3afb5087aa081c68ab7bde1e30bc5bb06735
qtgiebel <qtgiebel@gmail.com>
2022-05-09 03:27:30 -0500

    remove resource files from repo

---
+++ src/main/java/com/quinngiebel/portfolio/controls/RootDisplayServlet.java
@@ -0,0 +1,31 @@
+package com.quinngiebel.portfolio.controls;
+
+import com.quinngiebel.admin.persistence.CategoryDao;
+import com.quinngiebel.admin.persistence.PieceDao;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * This class forwards to a jsp page.
+ *
+ * @author Quinn Giebel
+ */
+@WebServlet(name = "RootDisplayServlet", urlPatterns = "")
+public class RootDisplayServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String forwardUrl = "/index.jsp";
+        PieceDao pieceDao = new PieceDao();
+
+        request.setAttribute("categories", new CategoryDao().getAll());
+
+        RequestDispatcher dispatcher = getServletContext().getRequestDispatcher(forwardUrl);
+        dispatcher.forward(request, response);
+    }
+}
